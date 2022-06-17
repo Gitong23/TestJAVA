c29a45758d987401ce78a0a1a3b4373215343533
Gitong23 <gitong23@gmail.com>
2022-06-17 21:23:37 +0700

    First

---
+++ TaxApplication/src/DUHEE.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class DUHEE extends JFrame{
+    private JPanel PanelHEE;
+    private JLabel HeeLabel;
+
+    public DUHEE(String title)
+    {
+        super(title);
+        this.setContentPane(PanelHEE);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+    }
+
+}
---
+++ TaxApplication/src/MainApp.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainApp extends JFrame{
+    private JTextField PricetextField;
+    private JButton CalButton;
+    private JLabel PriceTextLabel;
+    private JLabel ResultLabel;
+    private JPanel MainPanel;
+    private JButton DuheeBottom;
+
+    public MainApp(String title) {
+        super(title);
+        this.setContentPane(MainPanel);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+
+
+        CalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Double price = Double.parseDouble(PricetextField.getText());
+                price = price * 1.07;
+                ResultLabel.setText(price.toString() + "THB");
+
+            }
+        });
+        DuheeBottom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DUHEE duhee=new DUHEE("HeeYai");
+                duhee.setVisible(true);
+            }
+        });
+    }
+}
